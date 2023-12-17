6cf0750635f0bfa4be45b38b80e745f7fb10c66a
Jean Claude <aziaha.formations@gmail.com>
2023-12-17 17:52:31 +0100

    sprint2-ticket1-feat: Système d'inscription avec validation de compte mis en place.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,20 @@
 
 namespace App;
 
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use App\Trait\TimeZoneTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    use TimeZoneTrait;
+
+    public function __construct(string $environment, bool $debug)
+    {
+
+        $this->changeTimeZone("Europe/Paris");
+        
+        parent::__construct($environment, $debug);
+    }
 }
