a6ea2d2b6f15ec18e27f4cbce18e1a18adcabd43
Jean Claude <aziaha.formations@gmail.com>
2023-12-29 17:31:26 +0100

    sprint3_ticket7-feat: Les paramètres du site.

--- src/Controller/Visitor/Contact/ContactController.php
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Visitor\Contact;
 
 use App\Entity\Contact;
 use App\Form\ContactFormType;
+use App\Repository\SettingRepository;
 use App\Service\SendEmailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +15,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'visitor_contact_create', methods:['GET', 'POST'])]
-    public function create(Request $request, EntityManagerInterface $em, SendEmailService $sendEmailService): Response
+    public function create(
+        Request $request, 
+        EntityManagerInterface $em, 
+        SendEmailService $sendEmailService,
+        SettingRepository $settingRepository
+    ): Response
     {
         $contact = new Contact();
 
@@ -48,7 +54,8 @@ class ContactController extends AbstractController
         }
 
         return $this->render('pages/visitor/contact/create.html.twig', [
-            "form" => $form->createView()
+            "form" => $form->createView(),
+            "setting" => $settingRepository->find(1)
         ]);
     }
 }
