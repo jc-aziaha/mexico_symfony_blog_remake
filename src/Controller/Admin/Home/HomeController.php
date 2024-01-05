ec45791ecc048290c4b30520da9156e8a2845b54
Jean Claude <aziaha.formations@gmail.com>
2024-01-05 20:58:51 +0100

    sprint4-ticket6-add: Les grandes finitions.

--- src/Controller/Admin/Home/HomeController.php
+++ src/Controller/Admin/Home/HomeController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller\Admin\Home;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\Admin\Comment\CommentController;
+use App\Repository\TagRepository;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\CommentRepository;
+use App\Repository\ContactRepository;
+use App\Repository\LikeRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/admin/home', name: 'admin_home_index', methods:['GET'])]
-    public function index(): Response
+    public function index(
+        CategoryRepository $categoryRepository,
+        TagRepository $tagRepository,
+        PostRepository $postRepository,
+        ContactRepository $contactRepository,
+        UserRepository $userRepository,
+        CommentRepository $commentRepository,
+        LikeRepository $likeRepository
+    ): Response
     {
-        return $this->render('pages/admin/home/index.html.twig');
+        return $this->render('pages/admin/home/index.html.twig', [
+            "categories" => $categoryRepository->findAll(),
+            "tags"       => $tagRepository->findAll(),
+            "posts"      => $postRepository->findAll(),
+            "contacts"   => $contactRepository->findAll(),
+            "users"      => $userRepository->findAll(),
+            "comments"   => $commentRepository->findAll(),
+            "likes"      => $likeRepository->findAll(),
+        ]);
     }
 }
